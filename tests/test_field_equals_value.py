9fc3523e00e597a04d8fc6df21f46bb0fadcf97f
Ivan R <vanyaryanichev@gmail.com>
2024-09-07 20:23:42 +1000

    Switch to self assert

--- tests/test_field_equals_value.py
+++ tests/test_field_equals_value.py
@@ -12,11 +12,12 @@ class EqualsConditionTestCase(unittest.TestCase):
         lq = ListQuery(data).filter(first_name=self.test_first_name)
 
         for i in lq:
-            assert (i["first_name"] == self.test_first_name)
+            self.assertEqual(i["first_name"], self.test_first_name)
 
         donalts = [i for i in data if i["first_name"] == self.test_first_name]
-        assert (lq == donalts)
-        assert (len(lq))
+
+        self.assertEqual(lq, donalts)
+        self.assertTrue(len(lq))
 
     def test_exclude_equals_value(self):
         lq = ListQuery(data).exclude(first_name=self.test_first_name)
@@ -32,38 +33,38 @@ class EqualsConditionTestCase(unittest.TestCase):
         lq = ListQuery(data).filter(first_name=self.test_first_name, car_make="Toyota")
 
         for i in lq:
-            assert (i["first_name"] == self.test_first_name)
-            assert (i["car_make"] == "Toyota")
+            self.assertEqual(i["first_name"], self.test_first_name)
+            self.assertEqual(i["car_make"], "Toyota")
 
         donalts_toyota_drivers = [i for i in data if
                                   i["first_name"] == self.test_first_name and i["car_make"] == "Toyota"]
-        assert (lq == donalts_toyota_drivers)
-        assert (len(lq))
+        self.assertEqual(lq, donalts_toyota_drivers)
+        self.assertTrue(len(lq))
 
     def test_exclude_equals_multiple_values(self):
         lq = ListQuery(data).exclude(first_name=self.test_first_name, car_make="Toyota")
 
         for i in lq:
-            assert (i["first_name"] != self.test_first_name or i["car_make"] != "Toyota")
+            self.assertTrue(i["first_name"] != self.test_first_name or i["car_make"] != "Toyota")
 
         no_donalts_toyota_drivers = [i for i in data if
                                      i["first_name"] != self.test_first_name or i["car_make"] != "Toyota"]
 
-        assert (lq == no_donalts_toyota_drivers)
-        assert (len(lq))
+        self.assertEqual(lq, no_donalts_toyota_drivers)
+        self.assertTrue(len(lq))
 
     def test_nested_exclude_conditions(self):
         lq = ListQuery(data).exclude(first_name=self.test_first_name).exclude(car_make="Toyota")
 
         for i in lq:
-            assert (i["first_name"] != self.test_first_name)
-            assert (i["car_make"] != "Toyota")
+            self.assertNotEqual(i["first_name"], self.test_first_name)
+            self.assertNotEqual(i["car_make"], "Toyota")
 
         no_donalts_toyota_drivers = [i for i in data if
                                      i["first_name"] != self.test_first_name and i["car_make"] != "Toyota"]
 
-        assert (lq == no_donalts_toyota_drivers)
-        assert (len(lq))
+        self.assertEqual(lq, no_donalts_toyota_drivers)
+        self.assertTrue(len(lq))
 
 
 if __name__ == '__main__':
