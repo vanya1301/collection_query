a03c4e94c3a470a536fc57d61ff18c96069b3269
Ivan R <vanyaryanichev@gmail.com>
2024-10-11 19:53:29 +1100

    Add comment

--- list_query.py
+++ list_query.py
@@ -11,7 +11,7 @@ class ListQuery(list):
         return self._delete_condition_matching_items(True, **kwargs)
 
     def _delete_condition_matching_items(self, exclude=False, **kwargs) -> ListQuery:
-        for i in range(len(self) - 1, -1, -1):
+        for i in range(len(self) - 1, -1, -1):  # going in reverse cause we deleting items
             if hasattr(self[i], "get") and callable(self[i].get):
                 condition_result = all([self._evaluate_condition(self[i], k, v) for k, v in kwargs.items()])
                 condition_result = condition_result if exclude else not condition_result
